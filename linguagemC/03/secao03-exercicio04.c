4c49725707ca22028b83928fe279314f6b56a79d
Gustavo Brandão Gimenez <70249205+gustavo-brandao@users.noreply.github.com>
2022-06-07 21:19:45 -0400

    Exercício 05
    exercício 05 da seção 03

--- linguagemC/03/secao03-exercicio04.c
+++ linguagemC/03/secao03-exercicio04.c
@@ -5,7 +5,7 @@ int main(){
     int num1, num2, soma;
 
     //Entradas
-    prinf("Informe o primeiro numero: ");
+    printf("Informe o primeiro numero: ");
     scanf("%d", &num1);
     printf("Informe o segundo numero: ");
     scanf("%d", &num2);
@@ -13,5 +13,5 @@ int main(){
     //Processamento
     soma = (num1 + num2);
 
-    prinf("A soma eh: %d", soma);
+    printf("A soma eh: %d", soma);
 }
---
+++ linguagemC/04/enunciado.md
@@ -0,0 +1,9 @@
+Seção 03 - exercício 5
+
+Faça um algorítmo que converta metros para centímetros.
+
+"converter metros" -> centímetros
+
+receber o tamanho em metros
+multiplicar o tamanho por 100
+mostrar o resultado da multiplicação
